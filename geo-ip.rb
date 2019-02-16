f2225b74d7afe29bc86ca8edb928371e6746aefc
Gonçalo Ferreira <gonmf@sapo.pt>
2019-02-16 19:04:05 +0000

    Add .gitignore

--- geo-ip.rb
+++ geo-ip.rb
@@ -22,23 +22,8 @@ def load_names_database(lang)
   ret
 end
 
-def load_v4_blocks_database
-  filename = './db/GeoLite2-City-CSV_20190212/GeoLite2-City-Blocks-IPv4.csv'
-  puts "Loading #{filename}"
-
-  ret = {}
-
-  CSV.foreach(filename, headers: true) do |row|
-    id = row['network']
-
-    ret[id] = row['geoname_id'].to_i
-  end
-
-  ret
-end
-
-def load_v6_blocks_database
-  filename = './db/GeoLite2-City-CSV_20190212/GeoLite2-City-Blocks-IPv6.csv'
+def load_blocks_database(ip_version)
+  filename = "./db/GeoLite2-City-CSV_20190212/GeoLite2-City-Blocks-IP#{ip_version}.csv"
   puts "Loading #{filename}"
 
   ret = {}
@@ -54,8 +39,8 @@ end
 
 puts 'May take up to two minutes to load'
 en_names_db = load_names_database('en')
-v4_blocks_db = load_v4_blocks_database
-v6_blocks_db = load_v6_blocks_database
+v6_blocks_db = load_blocks_database('v6')
+v4_blocks_db = load_blocks_database('v4')
 
 set :bind, '0.0.0.0'
 set :show_exceptions, false
@@ -65,36 +50,27 @@ set :static, false
 set :dump_errors, false
 set :lock, false
 
-get '/en/v4/:ip' do |ip|
+get '/en/:ip_version/:ip' do |ip_version, ip|
   content_type :json
 
   geoname_id = nil
 
-  (8..32).reverse_each do |mask_digits|
-    geoname_id ||= begin
-      ip_w_mask = "#{IPAddr.new("#{ip}/#{mask_digits}").to_s}/#{mask_digits}"
+  case ip_version
+  when '4', 'v4'
+    (8..32).reverse_each do |mask_digits|
+      geoname_id ||= begin
+        ip_w_mask = "#{IPAddr.new("#{ip}/#{mask_digits}").to_s}/#{mask_digits}"
 
-      v4_blocks_db[ip_w_mask]
+        v4_blocks_db[ip_w_mask]
+      end
     end
-  end
+  when '6', 'v6'
+    (19..128).reverse_each do |mask_digits|
+      geoname_id ||= begin
+        ip_w_mask = "#{IPAddr.new("#{ip}/#{mask_digits}").to_s}/#{mask_digits}"
 
-  return [404, 'Unable to resolve'] if geoname_id.nil?
-
-  [200, en_names_db[geoname_id]]
-rescue
-  400
-end
-
-get '/en/v6/:ip' do |ip|
-  content_type :json
-
-  geoname_id = nil
-
-  (19..128).reverse_each do |mask_digits|
-    geoname_id ||= begin
-      ip_w_mask = "#{IPAddr.new("#{ip}/#{mask_digits}").to_s}/#{mask_digits}"
-
-      v6_blocks_db[ip_w_mask]
+        v6_blocks_db[ip_w_mask]
+      end
     end
   end
 
