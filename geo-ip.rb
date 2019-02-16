838c5f5cc88fd74d1a5024bdb99fb73bceeb597e
Gonçalo Ferreira <gonmf@sapo.pt>
2019-02-16 18:24:10 +0000

    Add IPv6

--- geo-ip.rb
+++ geo-ip.rb
@@ -2,21 +2,9 @@ require 'csv'
 require 'json'
 require 'sinatra'
 
-def generate_masked_ips(ip)
-  ips = []
-
-  (8..32).reverse_each do |mask_digits|
-    ips.push("#{IPAddr.new("#{ip}/#{mask_digits}").to_s}/#{mask_digits}")
-  end
-
-  ips.reverse
-rescue
-  []
-end
-
 def load_names_database(lang)
   filename = "./db/GeoLite2-City-CSV_20190212/GeoLite2-City-Locations-#{lang}.csv"
-  puts filename
+  puts "Loading #{filename}"
 
   ret = {}
 
@@ -34,9 +22,24 @@ def load_names_database(lang)
   ret
 end
 
-def load_blocks_database
+def load_v4_blocks_database
   filename = './db/GeoLite2-City-CSV_20190212/GeoLite2-City-Blocks-IPv4.csv'
-  puts filename
+  puts "Loading #{filename}"
+
+  ret = {}
+
+  CSV.foreach(filename, headers: true) do |row|
+    id = row['network']
+
+    ret[id] = row['geoname_id'].to_i
+  end
+
+  ret
+end
+
+def load_v6_blocks_database
+  filename = './db/GeoLite2-City-CSV_20190212/GeoLite2-City-Blocks-IPv6.csv'
+  puts "Loading #{filename}"
 
   ret = {}
 
@@ -51,8 +54,10 @@ end
 
 puts 'May take up to two minutes to load'
 en_names_db = load_names_database('en')
-blocks_db = load_blocks_database
+v4_blocks_db = load_v4_blocks_database
+v6_blocks_db = load_v6_blocks_database
 
+set :bind, '0.0.0.0'
 set :show_exceptions, false
 set :sessions, false
 set :logging, false
@@ -69,11 +74,31 @@ get '/en/v4/:ip' do |ip|
     geoname_id ||= begin
       ip_w_mask = "#{IPAddr.new("#{ip}/#{mask_digits}").to_s}/#{mask_digits}"
 
-      blocks_db[ip_w_mask]
+      v4_blocks_db[ip_w_mask]
     end
   end
 
-  return 404 if geoname_id.nil?
+  return [404, 'Unable to resolve'] if geoname_id.nil?
+
+  [200, en_names_db[geoname_id]]
+rescue
+  400
+end
+
+get '/en/v6/:ip' do |ip|
+  content_type :json
+
+  geoname_id = nil
+
+  (19..128).reverse_each do |mask_digits|
+    geoname_id ||= begin
+      ip_w_mask = "#{IPAddr.new("#{ip}/#{mask_digits}").to_s}/#{mask_digits}"
+
+      v6_blocks_db[ip_w_mask]
+    end
+  end
+
+  return [404, 'Unable to resolve'] if geoname_id.nil?
 
   [200, en_names_db[geoname_id]]
 rescue
